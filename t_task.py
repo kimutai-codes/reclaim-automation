ea6ee11876469bcb1d92e8f37daf211a6969cfd2
kimuati-codes <allankimutaikoech@gmail.com>
2024-06-23 14:21:41 +0300

    Revert "adsad"
    This reverts commit 8773660fd861aa5f90b9b7127d844a79b0d7cbb3.

--- t_task.py
+++ t_task.py
@@ -2,14 +2,9 @@ from reclaim_sdk.client import ReclaimClient
 from reclaim_sdk.models.task import ReclaimTask
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 token = os.getenv("RECLAIM_TOKEN")
 
 reclaim_client = ReclaimClient(token=token)
 
-# task = ReclaimTask.search(title="test_task_01")
-task = ReclaimTask.get(6801408)
+task = ReclaimTask.search(title="test_task_01")
 print(task)
